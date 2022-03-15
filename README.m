1bb9f090e4642af8765039c608826987cc8ca9da
Sisu-Yunus <101631633+Sisu-Yunus@users.noreply.github.com>
2022-03-15 09:18:45 +0000

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Sisu-Yunus
+- 👀 I’m interested in  programming
+- 🌱 I’m currently learning java
+- 💞️ I’m looking to collaborate on projects yet to be mentioned
+- 📫 How to reach me yunussisu@gmail.com
+
+<!---
+Sisu-Yunus/Sisu-Yunus is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
